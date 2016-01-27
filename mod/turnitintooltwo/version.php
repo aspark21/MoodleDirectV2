ac1ccc436d3c72acfbd5189bf34bb60e02f377ff
Alistair Spark <aspark21@users.noreply.github.com>
2016-01-27 18:08:40 +0000

    Make version Moodle 3.0 compatible

--- mod/turnitintooltwo/version.php
+++ mod/turnitintooltwo/version.php
@@ -18,18 +18,9 @@
  * @package   turnitintooltwo
  * @copyright 2012 iParadigms LLC
  */
-
-$module->version   = 2014012413;  // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2012062500;
-$module->component = 'mod_turnitintooltwo';
-$module->maturity  = MATURITY_STABLE;
-$module->release  = '2.3+';
-$module->cron      = 1800;        // Period for cron to check this module in seconds
-
 if (empty($plugin)) {
 	$plugin = new StdClass();
 }
-
 $plugin->version   = 2014012413;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012062500;
 $plugin->component = 'mod_turnitintooltwo';
